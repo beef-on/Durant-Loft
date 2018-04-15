2d47b72270f4bcb5336b5bbec6854fdf7bd37237
Meltingmettle <emmettl@berkeley.edu>
2018-04-14 21:15:12 -0700

    Updated HTML

--- DurantLoftApp/config/routes.rb
+++ DurantLoftApp/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :shifts
   resources :items
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+  
 end
