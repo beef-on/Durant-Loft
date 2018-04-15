8113d7677b9f7b7ecd320b45cddac2fa4a2b43da
JoshieBoshie <joshua91214@berkeley.edu>
2018-04-14 20:39:07 -0700

    just played with the forms a little bit

--- DurantLoftApp/config/routes.rb
+++ DurantLoftApp/config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :shifts
   resources :items
   resources :users
+  root to: "items#new"
+  get "shifts/new", to: "shifts#new"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
