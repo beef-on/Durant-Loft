a708c899029467c872d41e5afa73dc8f976c464f
JoshieBoshie <joshua91214@berkeley.edu>
2018-04-21 23:46:46 -0700

    i'm not sure if a lot of this works yet, but right now, I created the button the sign up. Probably want to work on getting people to log in and stay logged in.

--- DurantLoftApp/config/routes.rb
+++ DurantLoftApp/config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   root to: "home#index"
   get "shifts/new", to: "shifts#new"
   patch "home/bum", to: "users#bum", as: "bum"
-
+  patch "home/form", to: "users#new", as: "form"
+  get "home/form", to: "users#new"
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
