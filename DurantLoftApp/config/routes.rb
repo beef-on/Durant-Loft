7d0a3aa6be54eff0f4adf335c230ecdf515afce9
Joshua Asuncion <jasuncion@berkeley.edu>
2018-04-22 00:11:12 -0700

    front end stuff

--- DurantLoftApp/config/routes.rb
+++ DurantLoftApp/config/routes.rb
@@ -10,6 +10,5 @@ Rails.application.routes.draw do
   patch "home/bum", to: "users#bum", as: "bum"
 
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
