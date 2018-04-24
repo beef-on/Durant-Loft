04cbd7150a177d23df78b01ee33642d255a6e6f8
Brandon Fong <brandyfongfong@berkeley.edu>
2018-04-24 00:57:02 -0700

    started shifts

--- DurantLoftApp/app/models/shift.rb
+++ DurantLoftApp/app/models/shift.rb
@@ -1,5 +1,5 @@
 class Shift < ApplicationRecord
-  belongs_to :manner1
-  belongs_to :manner2
-  belongs_to :manner3
+  belongs_to :manner1, optional: true
+  belongs_to :manner2, optional: true
+  belongs_to :manner3, optional: true
 end
