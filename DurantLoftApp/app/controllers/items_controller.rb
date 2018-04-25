467c91fb2f7a17ced786dc520d9615d541d63ab5
Meltingmettle <emmettl@berkeley.edu>
2018-04-24 23:50:27 -0700

    Fixed inventory front end

--- DurantLoftApp/app/controllers/items_controller.rb
+++ DurantLoftApp/app/controllers/items_controller.rb
@@ -64,7 +64,7 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+      format.html { redirect_to inventory_path, notice: 'Item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
