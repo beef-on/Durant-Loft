8ddb7834de16e1e79e914586ae2eb1eccd32ef6c
Wilson Wang <wilsonw926@gmail.com>
2018-06-21 21:42:00 -0700

    sign up page and heand and header rendering

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module DurantLoftApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+	config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
