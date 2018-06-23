42dc4c471cd14aeb4cc1a52601db1c3cec112b01
Wilson Wang <wilsonw926@gmail.com>
2018-06-22 23:46:04 -0700

    sign up fix password confirm, edit user now works, fix driver sign up bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-       devise_parameter_sanitizer.permit(:sign_up, keys: [:firstName, :lastName, :year, :email, :password, :manner])
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:firstName, :lastName, :year, :email, :password, :manner, :driver])
+       devise_parameter_sanitizer.permit(:account_update, keys: [:firstName, :lastName, :year, :email, :manner, :driver])
     end
 end
